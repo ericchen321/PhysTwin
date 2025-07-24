cd843159ff0d2e121aa45d8c2c1dce1e50d27b4f
GanidhuAbey <ganidhu.abey@gmail.com>
2025-07-24 16:38:43 -0700

    added arguments to optionally control how many subsets are processed

--- script_optimize.py
+++ script_optimize.py
@@ -2,11 +2,32 @@ import glob
 import os
 import json
 
+import argparse
+
 base_path = "./data/different_types"
 dir_names = glob.glob(f"{base_path}/*")
-for dir_name in dir_names:
-    case_name = dir_name.split("/")[-1]
-    
+
+p = argparse.ArgumentParser()
+p.add_argument("--base_path", type=str, default=base_path)
+p.add_argument("--case_name", type=str, required=True)
+
+base_path = p.parse_args().base_path
+case_name = p.parse_args().case_name
+
+if (case_name == "all"):
+    for dir_name in dir_names:
+        case_name = dir_name.split("/")[-1]
+        
+        # Read the train test split
+        with open(f"{base_path}/{case_name}/split.json", "r") as f:
+            split = json.load(f)
+
+        train_frame = split["train"][1]
+
+        os.system(
+            f"python optimize_cma.py --base_path {base_path} --case_name {case_name} --train_frame {train_frame}"
+        )
+else:
     # Read the train test split
     with open(f"{base_path}/{case_name}/split.json", "r") as f:
         split = json.load(f)
