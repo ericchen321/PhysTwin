c2aa8d7d2043eba301d0c75437db37ccbb09ddf3
GanidhuAbey <ganidhu.abey@gmail.com>
2025-07-24 16:42:29 -0700

    added entity flag to control where wandb logs are stored

--- script_optimize.py
+++ script_optimize.py
@@ -10,9 +10,14 @@ dir_names = glob.glob(f"{base_path}/*")
 p = argparse.ArgumentParser()
 p.add_argument("--base_path", type=str, default=base_path)
 p.add_argument("--case_name", type=str, required=True)
+p.add_argument("--wandb_entity", type=str, required=True)
 
 base_path = p.parse_args().base_path
 case_name = p.parse_args().case_name
+
+# Set entity to control where wandb logs are stored
+entity = p.parse_args().wandb_entity
+os.environ["WANDB_ENTITY"] = entity
 
 if (case_name == "all"):
     for dir_name in dir_names:
--- script_train.py
+++ script_train.py
@@ -8,9 +8,14 @@ base_path = "./data/different_types"
 p = argparse.ArgumentParser()
 p.add_argument("--base_path", type=str, default=base_path)
 p.add_argument("--case_name", type=str, required=True)
+p.add_argument("--wandb_entity", type=str, required=True)
 
 base_path = p.parse_args().base_path
 case_name = p.parse_args().case_name
+
+# Set entity to control where wandb logs are stored
+entity = p.parse_args().wandb_entity
+os.environ["WANDB_ENTITY"] = entity
 
 if (case_name == "all"):
     dir_names = glob.glob(f"{base_path}/*")
