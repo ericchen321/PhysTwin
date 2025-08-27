3fa0281e03f6718a90767eb949c00d76d36bb262
eric <cgx1997@outlook.com>
2025-08-27 14:11:11 -0700

    explicitly set warp to 1.7.1; add missing packages

--- env_install/env_install.sh
+++ env_install/env_install.sh
@@ -1,11 +1,13 @@
 conda install -y numpy==1.26.4
-pip install warp-lang
+pip install warp-lang==1.7.1
 pip install usd-core matplotlib
 pip install "pyglet<2"
 pip install open3d
 pip install trimesh
 pip install rtree 
 pip install pyrender
+pip install plyfile==1.1.2
+pip install einops==0.8.1
 
 conda install -y pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 pip install stannum
@@ -19,25 +21,25 @@ pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.co
 
 # Install the env for realsense camera
 pip install Cython
-pip install pyrealsense2
+# pip install pyrealsense2
 pip install atomics
 pip install pynput
 
 # Install the env for grounded-sam-2
-pip install git+https://github.com/IDEA-Research/Grounded-SAM-2.git
-pip install git+https://github.com/IDEA-Research/GroundingDINO.git
+# pip install git+https://github.com/IDEA-Research/Grounded-SAM-2.git
+# pip install git+https://github.com/IDEA-Research/GroundingDINO.git
 
 # Install the env for image upscaler using SDXL
-pip install diffusers
-pip install accelerate
+# pip install diffusers
+# pip install accelerate
 
 # Install the env for trellis
-cd data_process
-git clone --recurse-submodules https://github.com/ericchen321/TRELLIS
-cd TRELLIS
-. ./setup.sh --basic --xformers --flash-attn --diffoctreerast --spconv --mipgaussian --kaolin --nvdiffrast
+# cd data_process
+# git clone --recurse-submodules https://github.com/microsoft/TRELLIS.git
+# cd TRELLIS
+# . ./setup.sh --basic --xformers --flash-attn --diffoctreerast --spconv --mipgaussian --kaolin --nvdiffrast
 
-cd ../..
+# cd ../..
 
 pip install gsplat==1.4.0
 pip install kornia
